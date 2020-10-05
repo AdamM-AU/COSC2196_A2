118abbf4abc9e739300fc2d3df1dd2a2d2fa0435
Adam <70795583+AdamM-AU@users.noreply.github.com>
2020-10-05 16:45:18 +1100

    Red Button
    Made "Red Button" toggle on and off with  a 15 sec penalty for pushing it the first time.

--- js/script.js
+++ js/script.js
@@ -39,9 +39,25 @@ $(document).ready(function(){
 /* Just for fun */
 
 function redbuttonFunction(link) {
-  document.body.style.backgroundImage = "url('images/bling.gif')";
-  if (link !== 1) {
-	alert("Didn't anyone tell you...\r\n DON'T PRESS THE RED BUTTON!")
-  }
-  return false;
+	if (link == 0) {
+		// first button push - turn on
+		alert("Didn't anyone tell you...\r\nDON'T PRESS THE RED BUTTON!\r\nWait 15 seconds to push button to disable.");
+		document.body.style.backgroundImage = "url('images/bling.gif')";
+		document.getElementById("red-button").setAttribute("onclick","return redbuttonFunction(3);");
+		
+		// Lock button for 10 seconds
+		setTimeout(function (){
+			document.getElementById("red-button").setAttribute("onclick","return redbuttonFunction(1);");
+		}, 15000);
+	
+	} else if (link == 3) {
+		alert("You Still Need To Wait!");
+		
+	} else {
+		// Second button push - turn off
+		document.getElementById("red-button").setAttribute("onclick","return redbuttonFunction(0);");
+		document.body.style.backgroundImage = "";
+	}
+
+	return false;
 }
