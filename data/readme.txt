60bde9994ff194db26ffb61939b25658f3629358
Adam <70795583+AdamM-AU@users.noreply.github.com>
2020-10-06 13:30:05 +1100

    JS function JSFetch
    Wrote a custom JavaScript + JQuery Function to import html files for the more complex data/page sections on the page.
    
    JSFetch(<class identifier>, <target document>, <output div id>)
    all target documents exist under <root directory/<folder>/<file>.html
    
    Added tab styles for menus, and place holder target documents

---
+++ data/readme.txt
@@ -0,0 +1,5 @@
+These folders will contain partial html files for complex / bulk data.
+JavaScript will import this data into the main page when required by the visitor/user.
+
+Regards,
+Adam
--- js/script.js
+++ js/script.js
@@ -36,8 +36,8 @@ $(document).ready(function(){
   });
 });
 
-/* Just for fun */
-
+/* RedButton - Just for fun */
+/* Adam Mutimer */
 function redbuttonFunction(link) {
 	if (link == 0) {
 		// first button push - turn on
@@ -59,5 +59,32 @@ function redbuttonFunction(link) {
 		document.body.style.backgroundImage = "";
 	}
 
+	return false;
+}
+
+/** JS Complex/Bulk Data Fetcher **/
+/** Adam Mutimer **/
+function JSFetch (nav,req,dest) {
+	if (!req || !dest) {
+		
+	} else {
+		// Use ajax to fetch the html file and process that data
+		$.ajax({url:'data/' + req + '.html', success: 
+			function (data){
+				if (data.length < 0) {
+					data = "Error: No Data Found";
+				} else {
+					// Push Data into destination
+					document.getElementById(dest).innerHTML = data;
+				}
+			}
+		});
+		// Update Sub Nav Tab
+		var navitem = req.substring(req.indexOf("/") + 1); // Drop data in string before "/" 
+		// Search for current Nav Menu + Active class and remove active class
+		document.getElementsByClassName(nav + " nav-link active")[0].classList.remove("active");
+		// use stipped navitem to set current selected menu item as active
+		document.getElementById(navitem).classList.add("active");
+	}
 	return false;
 }
