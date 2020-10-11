d36ec661e1db8e749cc1ca9b91413bc05be638f9
Channon Harper <s3871491@student.rmit.edu.au>
2020-10-11 18:15:18 +1000

    added 2 graphs as temp atm

--- js/jscharts-data.js
+++ js/jscharts-data.js
@@ -199,6 +199,46 @@ function JSFetchGraph(GRAPH) {
 					}
 				}
 			});
+		}else if (GRAPH == "GRAPH-ID"){
+			var mixedChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            label: 'Bar Dataset',
+            data: [10, 20, 30, 40],
+            // this dataset is drawn below
+            order: 1
+        }, {
+            label: 'Line Dataset',
+            data: [10, 10, 10, 10],
+            type: 'line',
+            // this dataset is drawn on top
+            order: 2
+        }],
+        labels: ['January', 'February', 'March', 'April']
+    },
+    options: options
+});
+		}else if (GRAPH == "GRAPH-ID2"){
+			var mixedChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            label: 'Bar Dataset',
+            data: [10, 20, 30, 40],
+            // this dataset is drawn below
+            order: 1
+        }, {
+            label: 'Line Dataset',
+            data: [10, 10, 10, 10],
+            type: 'line',
+            // this dataset is drawn on top
+            order: 2
+        }],
+        labels: ['January', 'February', 'March', 'April']
+    },
+    options: options
+});
 		} else {
 			// Do Nothing
 		}
