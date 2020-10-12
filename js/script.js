792e99540171c73def6510d8949da0c29c9c88c5
Adam <70795583+AdamM-AU@users.noreply.github.com>
2020-10-12 18:36:39 +1100

    Scroll to Top - Floating Button

--- js/script.js
+++ js/script.js
@@ -98,3 +98,20 @@ function JSFetch (nav,req,dest,graphobj) {
 	}
 	return false;
 }
+/* Scroll to Top - Floating Button */
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+});	
